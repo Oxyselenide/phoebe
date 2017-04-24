1a1097c6930cbba1408d290493290846350700d5
arunmj <arunmj001@gmail.com>
2017-04-24 08:12:41 +0530

    messing with SIDE MENU

--- phoebe/phoebe_web/static/js/layout/layout.controllers.js
+++ phoebe/phoebe_web/static/js/layout/layout.controllers.js
@@ -29,6 +29,43 @@
 
     SidebarController.inject = ['$scope'];
     function SidebarController($scope) {
+
+                                if (typeof jQuery === 'undefined') {
+            throw new Error('Theme\'s JavaScript requires jQuery');
+            }
+
+            var Side = {
+            _ps: $('.app-side'),
+            _body: $('body'),
+
+            responsive: function responsive() {
+                $(window).width() < 768 ? Side._body.removeClass('app-side-mini app-side-opened').addClass('app-side-closed') : Side._body.addClass('app-side-opened').removeClass('app-side-closed');
+                if (Side._body.hasClass('page-fixed') & !Side._body.hasClass('app-side-expand-on-hover')) 
+                Side._body.removeClass('app-side-mini');
+            }
+            };
+
+            $(document).ready(function(){
+            $(".metismenu").metisMenu();
+            
+            $(".side_menu_btn").click(function(event){
+            event.preventDefault();
+            if(this.id=="mini")
+                    Side._body.toggleClass('app-side-mini');
+                else
+                    Side._body.toggleClass('app-side-opened app-side-closed');
+                stopMetisMenu();
+            });
+            
+            function stopMetisMenu(){
+                $('.side-nav').find('li').removeClass('active');
+                $('.side-nav').find('a').attr('aria-expanded', false);
+                $('.side-nav').find('ul.collapse').removeClass('in').attr('aria-expanded', false);
+            }
+            alert("ok")
+            });
+
+
         activateMetisMenu()
 
         function activateMetisMenu(){
